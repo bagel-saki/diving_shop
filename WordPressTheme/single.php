c0b3ee9565d223318959346d3780787dcab198f6
sakko <sakko8pan@gmail.com>
2023-06-24 17:06:43 +0900

    画像のパスとページ内のヘッダー、フッターの削除

--- WordPressTheme/single.php
+++ WordPressTheme/single.php
@@ -1,175 +1,5 @@
 <?php get_header(); ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta name="robots" content="noindex">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <meta name="format-detection" content="telephone=no">
-  <!-- meta情報 -->
-  <title>投稿__diving_shop</title>
-  <meta name="description" content="ダイビングショップのブログ詳細ページです">
-  <meta property="og:site_name" content="投稿__diving_shop">
-  <meta property="og:url" content="https://aaa.com/blog/投稿/">
-  <meta property="og:type" content="website">
-  <meta property="og:title" content="投稿__diving_shop">
-  <meta property="og:description" content="ダイビングショップのブログ詳細ページです">
-  <meta property="og:locale" content="ja_JP">
-  <meta name="twitter:card" content="summary_large_image">
-  <!-- （画像関連） -->
-  <link rel="icon" href="#">
-  <meta property="og:image" content="./img/common/ogp.jpg">
-  <!-- フォント -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Gotu&family=Lato&family=Noto+Sans+JP:wght@400;500;700&family=Noto+Serif+JP:wght@400;500;700&display=swap" rel="stylesheet">
-  <!-- swiper -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
-  <!-- css -->
-  <link rel="stylesheet" href="./assets/css/style.css">
-  <!-- JavaScript -->
-  <script src="https://code.jquery.com/jquery-3.6.0.js" defer></script>
-  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js" defer></script>
-  <script src="./assets/js/script.js" defer></script>
-</head>
-<body>
-  <header class="p-header js-header">
-    <div class="p-header__inner">
-      <div class="p-header__container">
-        <a class="p-header__link" href="index.html">
-          <div class="p-header__logo">
-            <div class="c-logo"></div>
-          </div>
-        </a>
-        <div class="p-header__pc-nav">
-          <nav class="p-pc-nav  u-desktop">
-            <ul class="p-pc-nav__items">
-              <li class="p-pc-nav__item">
-                <a class="p-pc-nav__title" href="archive-campaign.html">
-                  <p class="p-pc-nav__en-title">Campaign</p>
-                  <p class="p-pc-nav__ja-title">キャンペーン</p>
-                  <span></span>
-                </a>
-              </li>
-              <li class="p-pc-nav__item">
-                <a class="p-pc-nav__title" href="page-about.html">
-                  <p class="p-pc-nav__en-title">About us</p>
-                  <p class="p-pc-nav__ja-title">私たちについて</p>
-                  <span></span>
-                </a>
-              </li>
-              <li class="p-pc-nav__item">
-                <a class="p-pc-nav__title" href="page-diving-info.html">
-                  <p class="p-pc-nav__en-title">Information</p>
-                  <p class="p-pc-nav__ja-title">ダイビング情報</p>
-                  <span></span>
-                </a>
-              </li>
-              <li class="p-pc-nav__item">
-                <a class="p-pc-nav__title" href="home.html">
-                  <p class="p-pc-nav__en-title">Blog</p>
-                  <p class="p-pc-nav__ja-title">ブログ</p>
-                  <span></span>
-                </a>
-              </li>
-              <li class="p-pc-nav__item">
-                <a class="p-pc-nav__title" href="archive-voices.html">
-                  <p class="p-pc-nav__en-title">Voice</p>
-                  <p class="p-pc-nav__ja-title">お客様の声</p>
-                  <span></span>
-                </a>
-              </li>
-              <li class="p-pc-nav__item">
-                <a class="p-pc-nav__title" href="page-price.html">
-                  <p class="p-pc-nav__en-title">Price</p>
-                  <p class="p-pc-nav__ja-title">料金一覧</p>
-                  <span></span>
-                </a>
-              </li>
-              <li class="p-pc-nav__item">
-                <a class="p-pc-nav__title" href="page-faq.html">
-                  <p class="p-pc-nav__en-title">FAQ</p>
-                  <p class="p-pc-nav__ja-title">よくある質問</p>
-                  <span></span>
-                </a>
-              </li>
-              <li class="p-pc-nav__item">
-                <a class="p-pc-nav__title" href="page-contact.html">
-                  <p class="p-pc-nav__en-title">Contact</p>
-                  <p class="p-pc-nav__ja-title">お問合せ</p>
-                  <span></span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div class="p-header__hamburger">
-          <div class="p-hamburger js-hamburger u-mobile">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        <div class="p-header__drawer">
-          <nav class="p-drawer js-drawer">
-            <div class="p-drawer__inner l-inner">
-              <div class="p-drawer__container">
-                <ul class="p-drawer__leftItems">
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="archive-campaign.html">キャンペーン</a>
-                    <div class="p-drawer__lists">
-                      <a class="p-drawer__list" href="">ライセンス取得</a>
-                      <a class="p-drawer__list" href="">貸切体験ダイビング</a>
-                      <a class="p-drawer__list" href="">ナイトダイビング</a>
-                    </div>
-                  </li>
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="page-about.html">私たちについて</a>
-                  </li>
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="page-diving-info.html">ダイビング情報</a>
-                    <div class="p-drawer__lists">
-                      <a class="p-drawer__list" href="">ライセンス講習</a>
-                      <a class="p-drawer__list" href="">体験ダイビング</a>
-                      <a class="p-drawer__list" href="">ファンダイビング</a>
-                    </div>
-                  </li>
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="home.html">ブログ</a>
-                  </li>
-                </ul>
-                <ul class="p-drawer__rightItems">
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="archive-voices.html">お客様の声</a>
-                  </li>
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="page-price.html">料金一覧</a>
-                    <div class="p-drawer__lists">
-                      <a class="p-drawer__list" href="">ライセンス講習</a>
-                      <a class="p-drawer__list" href="">体験ダイビング</a>
-                      <a class="p-drawer__list" href="">ファンダイビング</a>
-                    </div>
-                  </li>
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="page-faq.html">よくある質問</a>
-                  </li>
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="page-privacy-policy.html">プライバシー<br>ポリシー</a>
-                  </li>
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="page-terms.html">利用規約</a>
-                  </li>
-                  <li class="p-drawer__item">
-                    <a class="p-drawer__title" href="page-contact.html">お問い合わせ</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-  </header>
+
   <main>
     <div class="p-lower-mv p-lower-mv--blog">
       <div class="p-lower-mv__content">
@@ -201,11 +31,11 @@
                   </div>
                   <div class="p-lower-singleBlog">
                     <div>
-                      <img src="./assets/images/common/blog1.jpg" alt="" />
+                      <img src="<?php echo get_template_directory_uri(); ?>./assets/images/common/blog1.jpg" alt="" />
                     </div>
                     <p> ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。 </p>
                     <div>
-                      <img src="./assets/images/common/blog1.jpg" alt="" />
+                      <img src="<?php echo get_template_directory_uri(); ?>./assets/images/common/blog1.jpg" alt="" />
                     </div>
                     <p> ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。 </p>
                     <nav>
@@ -256,7 +86,7 @@
                           <a class="p-sidebar-articleCard__link" href="single.html">
                             <div class="p-sidebar-articleCard__inner">
                               <div class="p-sidebar-articleCard__img">
-                                <img src="
+                                <img src="<?php echo get_template_directory_uri(); ?>
             ./assets/images/common/blog4.jpg" alt="海の中を泳ぐ黄色い魚">
                               </div>
                               <div class="p-sidebar-articleCard__body">
@@ -272,7 +102,7 @@
                           <a class="p-sidebar-articleCard__link" href="single.html">
                             <div class="p-sidebar-articleCard__inner">
                               <div class="p-sidebar-articleCard__img">
-                                <img src="
+                                <img src="<?php echo get_template_directory_uri(); ?>
             ./assets/images/common/blog3.jpg" alt="海の中を泳ぐ亀">
                               </div>
                               <div class="p-sidebar-articleCard__body">
@@ -288,7 +118,7 @@
                           <a class="p-sidebar-articleCard__link" href="single.html">
                             <div class="p-sidebar-articleCard__inner">
                               <div class="p-sidebar-articleCard__img">
-                                <img src="
+                                <img src="<?php echo get_template_directory_uri(); ?>
             ./assets/images/common/blog2.jpg" alt="イソギンチャクから顔を出すクマノミ">
                               </div>
                               <div class="p-sidebar-articleCard__body">
@@ -311,7 +141,7 @@
                     </div>
                     <div class="p-sidebar-review__content">
                       <div class="p-sidebar-review__img">
-                        <img src="./assets/images/common/voice_side.jpg" alt="ソファに座って腕を組み微笑む若いカップル" />
+                        <img src="<?php echo get_template_directory_uri(); ?>./assets/images/common/voice_side.jpg" alt="ソファに座って腕を組み微笑む若いカップル" />
                       </div>
                     </div>
                     <div class="p-sidebar-review__age">30代(カップル)</div>
@@ -337,7 +167,7 @@
                         <div class="p-sidebar-campaignCard">
                           <a class="p-sidebar-campaignCard__link" href="archive-campaign.html">
                             <div class="p-sidebar-campaignCard__img">
-                              <img src="./assets/images/common/campaign-card_1.jpg" alt="赤、青、黄等の小さな魚が海の中を泳いでいる">
+                              <img src="<?php echo get_template_directory_uri(); ?>./assets/images/common/campaign-card_1.jpg" alt="赤、青、黄等の小さな魚が海の中を泳いでいる">
                             </div>
                             <div class="p-sidebar-campaignCard__head">
                               <p class="p-sidebar-campaignCard__title">ライセンス取得</p>
@@ -357,7 +187,7 @@
                         <div class="p-sidebar-campaignCard">
                           <a class="p-sidebar-campaignCard__link" href="archive-campaign.html">
                             <div class="p-sidebar-campaignCard__img">
-                              <img src="./assets/images/common/campaign-card_2.jpg" alt="海岸を出発し島に向かう二艘の船">
+                              <img src="<?php echo get_template_directory_uri(); ?>./assets/images/common/campaign-card_2.jpg" alt="海岸を出発し島に向かう二艘の船">
                             </div>
                             <div class="p-sidebar-campaignCard__head">
                               <p class="p-sidebar-campaignCard__title">貸切体験ダイビング</p>
@@ -464,86 +294,5 @@
       </section>
     </div>
   </main>
-  <div class="l-footer">
-    <footer class="p-footer">
-      <div class="p-footer__inner l-inner">
-        <div class="p-footer__head">
-          <a href="index.html" class="p-footer__logo">
-            <div class="c-logo c-logo--large"></div>
-          </a>
-          <div class="p-footer__sns">
-            <img src="./assets/images/common/icon-facebook.png" alt="フェイスブック">
-            <img src="./assets/images/common/icon-instaglam.png" alt="インスタグラム">
-          </div>
-        </div>
-        <div class="p-footer__contents">
-          <div class="p-contents p-contents--footer">
-            <nav class="p-contents__container">
-              <ul class="p-contents__items">
-                <li class="p-contents__item">
-                  <a href="archive-campaign.html" class="p-contents__title p-contents__title--white"> キャンペーン </a>
-                  <div class="p-contents__lists">
-                    <a href="" class="p-contents__list">ライセンス取得</a>
-                    <a href="" class="p-contents__list">貸切体験ダイビング</a>
-                    <a href="" class="p-contents__list">ナイトダイビング</a>
-                  </div>
-                </li>
-                <li class="p-contents__item">
-                  <a href="page-about.html" class="p-contents__title p-contents__title--white"> 私たちについて </a>
-                </li>
-              </ul>
-              <ul class="p-contents__items">
-                <li class="p-contents__item">
-                  <a href="page-diving-info.html" class="p-contents__title p-contents__title--white"> ダイビング情報 </a>
-                  <div class="p-contents__lists">
-                    <a href="" class="p-contents__list">ライセンス講習</a>
-                    <a href="" class="p-contents__list">体験ダイビング</a>
-                    <a href="" class="p-contents__list">ファンダイビング</a>
-                  </div>
-                </li>
-                <li class="p-contents__item">
-                  <a href="home.html" class="p-contents__title p-contents__title--white"> ブログ </a>
-                </li>
-              </ul>
-              <ul class="p-contents__items">
-                <li class="p-contents__item">
-                  <a href="archive-voices.html" class="p-contents__title p-contents__title--white"> お客様の声 </a>
-                </li>
-                <li class="p-contents__item">
-                  <a href="page-price.html" class="p-contents__title p-contents__title--white"> 料金一覧 </a>
-                  <div class="p-contents__lists">
-                    <a href="" class="p-contents__list">ライセンス講習</a>
-                    <a href="" class="p-contents__list">体験ダイビング</a>
-                    <a href="" class="p-contents__list">ファンダイビング</a>
-                  </div>
-                </li>
-              </ul>
-              <ul class="p-contents__items">
-                <li class="p-contents__item">
-                  <a href="page-faq.html" class="p-contents__title p-contents__title--white"> よくある質問 </a>
-                </li>
-                <li class="p-contents__item">
-                  <a href="page-privacy-policy.html" class="p-contents__title p-contents__title--white"> プライバシー<br class="u-mobile">ポリシー </a>
-                </li>
-                <li class="p-contents__item">
-                  <a href="page-terms.html" class="p-contents__title p-contents__title--white"> 利用規約 </a>
-                </li>
-                <li class="p-contents__item">
-                  <a href="page-contact.html" class="p-contents__title p-contents__title--white"> お問い合わせ </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <div class="p-footer__copy">
-          <small class="p-footer__copyright">copyright&ensp;&copy;&ensp;2021&ensp;-&ensp;2023&ensp;code<span>u</span>ps&ensp;<span>llc.&ensp;</span>all&ensp;rights&ensp;reserved.</small>
-        </div>
-      </div>
-    </footer>
-  </div>
-  <div class="c-page-top-btn js-page-top" ontouchstart=""></div>
-</body>
-</html>
-
 
 <?php get_footer(); ?>
