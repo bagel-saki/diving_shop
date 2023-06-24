7775ae1418f08a813d7c6514b7491055f49feb59
sakko <sakko8pan@gmail.com>
2023-06-25 06:09:52 +0900

    パンくずの動的化（home.php以外）

--- WordPressTheme/404.php
+++ WordPressTheme/404.php
@@ -1,29 +1,24 @@
 <?php get_header(); ?>
 
-  <main>
-    <div class="l-lower-404">
-      <section class="p-404-section">
-        <div class="l-inner">
-          <nav class="breadcrumb breadcrumb--white">
-            <ul class="breadcrumb-list">
-              <li class="breadcrumb-item"><a href="index.html">top</a><span>&gt;</span></li>
-              <li class="breadcrumb-item active">404</li>
-            </ul>
-          </nav>
-        </div>
-        <div class="p-404-section__inner l-inner">
-          <h1 class="p-404-section__title">404</h1>
-          <div class="p-404-section__text"> 申し訳ありません。<br> お探しのページが見つかりません。 </div>
-          <div class="p-404-section__btn">
-            <div class="c-btn" ontouchstart="">
-              <a href="404.html" class="c-btn__link c-btn__link--white"> Page&nbsp;Top<span></span>
-              </a>
-              <div class="c-btn__overlay c-btn__overlay--white"></div>
-            </div>
+<main>
+  <div class="l-lower-404">
+    <section class="p-404-section">
+      <div class="l-inner">
+          <?php get_template_part('breadcrumb'); ?>
+      </div>
+      <div class="p-404-section__inner l-inner">
+        <h1 class="p-404-section__title">404</h1>
+        <div class="p-404-section__text"> 申し訳ありません。<br> お探しのページが見つかりません。 </div>
+        <div class="p-404-section__btn">
+          <div class="c-btn" ontouchstart="">
+            <a href="404.html" class="c-btn__link c-btn__link--white"> Page&nbsp;Top<span></span>
+            </a>
+            <div class="c-btn__overlay c-btn__overlay--white"></div>
           </div>
         </div>
-      </section>
-    </div>
-  </main>
+      </div>
+    </section>
+  </div>
+</main>
 
 <?php get_footer(); ?>
---
+++ WordPressTheme/breadcrumb.php
@@ -0,0 +1,7 @@
+<nav class="breadcrumb" vocab="http://schema.org/" typeof="BreadcrumbList">
+    <?php
+    if (function_exists('bcn_display')) {
+        bcn_display();
+    }
+    ?>
+</nav>
